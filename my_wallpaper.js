98ade10e6e9900128d306db8221453426e4dd3ad
Jasper Alani <jasperalani@gmail.com>
2024-08-02 17:46:24 +1200

    Cleaned up code
    Cleaned up code and removed unused variables/functions/files

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,8 +1,8 @@
-//your parameter variables go here!
 let rect_width = 200;
 let rect_height = 200;
 let middle = rect_width === rect_height ? rect_width / 2 : 0;
 
+// These are the colours im using
 const Colours = {
     ONE: "#FF99C8",
     TWO: "#FCF6BD",
@@ -11,6 +11,8 @@ const Colours = {
     FIVE: "#E4C1F9"
 };
 
+// These are the same colours just in RGBA format with
+// the end of the string missing so opacity can be added later
 const Colours_RGBA = {
     ONE: "rgba(255, 153, 200, ",
     TWO: "rgba(252, 246, 189, ",
@@ -19,18 +21,15 @@ const Colours_RGBA = {
     FIVE: "rgba(228, 193, 249, "
 };
 
-/*
-    For use with Colours_RGBA
- */
+// For use with Colours_RGBA
 function getColour(colour, opacity) {
     return colour + opacity + ")"
 }
 
 function setup_wallpaper(pWallpaper) {
-    // pWallpaper.output_mode(DEVELOP_GLYPH);
     pWallpaper.output_mode(GRID_WALLPAPER);
     pWallpaper.resolution(NINE_PORTRAIT);
-    pWallpaper.show_guide(false); //set this to false when you're ready to print
+    pWallpaper.show_guide(false);
 
     //Grid settings
     pWallpaper.grid_settings.cell_width = rect_width;
@@ -44,17 +43,10 @@ function wallpaper_background() {
     background("white");
 }
 
-/* inspiration: https://editor.p5js.org/yuletide/sketches/sSCcQ27F8 */
 function my_symbol() {
-    drawShapes();
-}
-
-function drawShapes() {
     /* VARIABLES */
-    const ThinWhiteStarPoints = 12;
     const directionalLinesStrokeWeight = 100;
     const directionalLinesDashed = false;
-    const PurpleQuadStarPoints = 2;
     const linesAreRects = false;
     const greenSquareSize = 100;
     const blueCornerStarPoints = 8;
@@ -63,6 +55,12 @@ function drawShapes() {
     const centerStarColour = Colours.THREE;
     const radiatingCircleDiameter = 75;
     const radiatingCircleOpacity = 1;
+    const drawFourCornerStars = false;
+    const PurpleQuadStarPoints = 2;
+    const ThinCenterStarPoints = 12;
+    const ThinCenterStarColour = getColour(Colours_RGBA.TWO, 0.75);
+    const drawThinCenterStar1 = true;
+    const drawThinCenterStar2 = true;
     /* END VARIABLES */
 
     strokeWeight(0)
@@ -77,13 +75,14 @@ function drawShapes() {
     push()
     strokeWeight(directionalLinesStrokeWeight)
     stroke(getColour(Colours_RGBA.ONE, 0.75))
-    if(directionalLinesDashed){
+    if (directionalLinesDashed) {
         drawingContext.setLineDash([5, 5]);
     }
     line(0, 0, rect_width, rect_height)
     line(rect_width, 0, 0, rect_height)
     pop()
 
+    // Green square at 45deg angle
     push()
     fill(Colours.THREE)
     rotate(-45)
@@ -96,9 +95,9 @@ function drawShapes() {
     drawStar(0, 0, 50, 40, blueCornerStarPoints)
     pop()
 
-    // white corner star
+    // White corner star
     push()
-    drawStar(0, 0, 25, 10, blueCornerStarPoints/2)
+    drawStar(0, 0, 25, 10, blueCornerStarPoints / 2)
     pop()
 
     // Background 45deg square
@@ -115,50 +114,54 @@ function drawShapes() {
     drawStar(middle, middle, 50, 20, centerStarPoints)
     pop()
 
-    // VERTICAL LINE
+    // Vertical circles
     push()
     fill(getColour(Colours_RGBA.FIVE, 1))
-    if(linesAreRects){
+    if (linesAreRects) {
         rect(0, 100, 10, 10)
-    }else{
-        ellipse(0, 100, 10, 10)
+    } else {
+        circle(0, 100, 10)
     }
     pop()
 
-    // HORIZONTAL LINE
+    // Horizontal circles
     push()
     fill(getColour(Colours_RGBA.FIVE, 1))
-    if(linesAreRects) {
+    if (linesAreRects) {
         rect(100, 0, 10, 10)
-    }else{
-        ellipse(100, 0, 10, 10)
+    } else {
+        circle(100, 0, 10)
     }
     pop()
 
     // Four corner stars
-    // push()
-    // fill(Colours.ONE)
-    // stroke("rgba(255, 255, 255, 0.75)")
-    // strokeWeight(0)
-    // const smallStarInner = 8;
-    // // drawStar(middle, middle,25, smallStarInner, PurpleQuadStarPoints)
-    // drawStar(50, 50, 25, smallStarInner, PurpleQuadStarPoints)
-    // drawStar(150, 50, 25, smallStarInner, PurpleQuadStarPoints*2)
-    // drawStar(50, 150, 25, smallStarInner, PurpleQuadStarPoints*2)
-    // drawStar(150, 150, 25, smallStarInner, PurpleQuadStarPoints)
-    // pop()
+    if (drawFourCornerStars) {
+        push()
+        fill(Colours.ONE)
+        stroke("rgba(255, 255, 255, 0.75)")
+        strokeWeight(0)
+        const smallStarInner = 8;
+        drawStar(50, 50, 25, smallStarInner, PurpleQuadStarPoints)
+        drawStar(150, 50, 25, smallStarInner, PurpleQuadStarPoints * 2)
+        drawStar(50, 150, 25, smallStarInner, PurpleQuadStarPoints * 2)
+        drawStar(150, 150, 25, smallStarInner, PurpleQuadStarPoints)
+        pop()
+    }
 
-    // Thin white star
-    // push()
-    // fill(getColour(Colours_RGBA.TWO, 0.75))
-    // // fill("rgba(255, 255, 255, 0.5)")
-    // // fill("white")
-    // drawStar(middle, middle, 75, 10, ThinWhiteStarPoints)
-    // pop()
 
-    // push()
-    // drawStar(middle, middle, 50, 10, 6)
-    // pop()
+    if(drawThinCenterStar1){
+        // Thin white star
+        push()
+        fill(ThinCenterStarColour)
+        drawStar(middle, middle, 75, 10, ThinCenterStarPoints)
+        pop()
+    }
+
+    if(drawThinCenterStar2){
+        push()
+        drawStar(middle, middle, 50, 10, 6)
+        pop()
+    }
 }
 
 /* inspiration: https://editor.p5js.org/p5/sketches/Form:_Star */
@@ -184,8 +187,4 @@ function drawStar(x, y, outerRadius, innerRadius, points) {
     }
 
     endShape(CLOSE);
-}
-
-function rand(min, max) {
-    return int(random(min, max));
 }
